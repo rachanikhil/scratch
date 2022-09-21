d6886644735b01d4d77b39edce9d4d1954c34eae
nikhil149 <nikhil123rachawar@gmail.com>
2022-09-21 12:08:21 +0530

    store updated with position and rotation

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import degreeSlice from "./degreeSlice";
 import positionSlice from "./positionSlice";
 
 const store = configureStore({
   reducer: {
     position: positionSlice.reducer,
+    rotation: degreeSlice.reducer,
   },
 });
 
--- src/store/positionSlice.js
+++ src/store/positionSlice.js
@@ -1,23 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialPositionState = {xPosition: 0, yPosition: 0};
+const initialPositionState = {
+  xPosition: 0,
+  yPosition: 0,
+  maxLeft: 0,
+  maxTop: 0,
+};
 
-const positionSlice= createSlice({
-    initialState: initialPositionState,
-    name: "positionSlice",
-    reducers: {
-        updatePositionX(state, action) {
-            state.xPosition += action.payload;
-        },
-        updatePositionY(state, action) {
-            state.yPosition += action.payload;
-        },
-        updateCatPosition(state, action) {
-            state.xPosition += action.payload.x;
-            state.yPosition += action.payload.y;
-        }
+const positionSlice = createSlice({
+  initialState: initialPositionState,
+  name: "positionSlice",
+  reducers: {
+    updatePositionX(state, action) {
+      state.xPosition += action.payload;
+    },
+    updatePositionY(state, action) {
+      state.yPosition += action.payload;
+    },
+    updateCatPosition(state, action) {
+      state.xPosition = action.payload.x;
+      state.yPosition = action.payload.y;
+    },
+    updateMaxPosition(state, action) {
+       state.maxLeft = action.payload.left;
+       state.maxTop = action.payload.top; 
     }
-})
+  },
+});
 
 export const positionSliceActions = positionSlice.actions;
 
