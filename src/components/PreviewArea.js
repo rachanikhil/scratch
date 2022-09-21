3bf7c8a708fa202cf591cedbead7939b26826c98
nikhil149 <nikhil123rachawar@gmail.com>
2022-09-21 18:26:37 +0530

    Conditional rendering done for hello and hmm

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -1,9 +1,24 @@
 import React from "react";
 import CatSprite from "./CatSprite";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { lookSliceActions } from "../store/lookSlice";
 
 export default function PreviewArea() {
+  const looks = useSelector((state) => state.looks);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const greetTime = setTimeout(() => {
+      if (looks.duration >= 0) {
+        dispatch(lookSliceActions.updateIsGreetClicked(false));
+      }
+    }, looks.duration * 1000);
+
+    return () => clearTimeout(greetTime);
+  }, [looks.isGreetClicked]);
   return (
     <div className="flex-none h-full overflow-y-auto p-2">
+      {looks.isGreetClicked && <p>{looks.text}</p>}
       <CatSprite />
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import degreeSlice from "./degreeSlice";
+import lookSlice from "./lookSlice";
 import positionSlice from "./positionSlice";
 
 const store = configureStore({
   reducer: {
     position: positionSlice.reducer,
     rotation: degreeSlice.reducer,
+    looks: lookSlice.reducer,
   },
 });
 
