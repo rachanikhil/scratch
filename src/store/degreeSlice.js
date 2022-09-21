a05d1edae7612ba8118859ea4d6f946d3310722e
nikhil149 <nikhil123rachawar@gmail.com>
2022-09-21 12:07:06 +0530

    rotation slice created

---
+++ src/store/degreeSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialDegreeState = {degree: 0};
+
+const degreeSlice= createSlice({
+    initialState: initialDegreeState,
+    name: "degreeSlice",
+    reducers: {
+        updateDegree(state, action) {
+            state.degree += action.payload;
+        }
+    }
+})
+
+export const degreeSliceActions = degreeSlice.actions;
+
+export default degreeSlice;
