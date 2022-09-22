04992a2167a7f970fd8d18abe35d077a1fd409e5
nikhil149 <nikhil123rachawar@gmail.com>
2022-09-22 10:49:35 +0530

    half functionality added in mid area

--- src/components/MidArea.js
+++ src/components/MidArea.js
@@ -2,24 +2,41 @@ import React from "react";
 import { useState } from "react";
 
 export default function MidArea(props) {
-  const [tasks, setTasks] = useState([]);
+  const [names, setNames] = useState([]);
   const onDragOver = (ev) => {
     ev.preventDefault();
     ev.dataTransfer.dropEffect = "copy";
   };
 
-  const onDrop = (ev, cat) => {
+  const onDrop = (ev) => {
     let id = ev.dataTransfer.getData("id");
+    ev.dataTransfer.dropEffect = "copy";
     const element = document.getElementById(id);
     ev.target.appendChild(element);
+    setNames((prevState) => [...prevState, id]);
+  };
+  const catEvents = (e) => {
+    names.forEach((name) => {
+      let element = document.getElementById(name);
+      element.click();
+    });
   };
   return (
-    <div
-      className="flex-1 h-full overflow-auto"
-      onDragOver={(e) => onDragOver(e)}
-      onDrop={(e) => onDrop(e, "complete")}
-    >
-      {props.children}
+    <div className="flex-1 h-full overflow-auto">
+      <div
+        className="flex-1 h-full overflow-auto"
+        onDragOver={(e) => onDragOver(e)}
+        onDrop={(e) => onDrop(e)}
+      >
+        {props.children}
+        <button
+          className="bg-green-500 text-white"
+          type="button"
+          onClick={(e) => catEvents(e)}
+        >
+          Run Cat
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,8 +5,24 @@ import Looks from "./Looks";
 import Motion from "./Motion";
 
 export default function Sidebar() {
+  const onDragOver = (ev) => {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "CopyOrMove";
+  };
+
+  const onDrop = (ev, cat) => {
+    let id = ev.dataTransfer.getData("id");
+    ev.dataTransfer.dropEffect = "CopyOrMove";
+    const element = document.getElementById(id);
+    ev.target.appendChild(element);
+  };
   return (
-    <div className="w-60 flex-none h-full overflow-y-auto flex flex-col items-start p-2 border-r border-gray-200">
+    <div
+      onDragOver={(e) => onDragOver(e)}
+      onDrop={(e) => onDrop(e)}
+      className="w-60 flex-none h-full overflow-y-auto flex flex-col items-start
+      p-2 border-r border-gray-200"
+    >
       <Events />
       <Motion />
       <Looks />
